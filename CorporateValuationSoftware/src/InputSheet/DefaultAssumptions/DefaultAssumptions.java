f33d8e49130bd67de90d50b90f6b60b67493ee41
scykl1 <scykl1@nottingham.edu.cn>
2022-10-28 16:57:58 +0800

    refresh the JAVA framework

--- CorporateValuationSoftware/src/InputSheet/DefaultAssumptions/DefaultAssumptions.java
+++ CorporateValuationSoftware/src/InputSheet/DefaultAssumptions/DefaultAssumptions.java
@@ -1,6 +1,171 @@
 package InputSheet.DefaultAssumptions;
 
 public class DefaultAssumptions {
+    public DefaultAssumptions(boolean override_stable_growth_A41, float if_yes_enter_the_cost_of_capital_after_year10_A42, boolean override_competitive_advantages_will_fade_A44, float if_yes_enter_capital_you_expect_after_year10_A45, boolean override_firm_has_no_chance_of_failure_A47, float if_yes_enter_the_probability_of_failure_A48, String what_do_you_want_to_tie_your_proceeds_in_failure_to_A49, float enter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50, boolean override_long_term_tax_rate_assumption_A52, boolean overrude_loss_tax_shield_assumption_A54, float if_yes_enter_NOL_carrying_into_year1_A55, boolean override_long_term_risk_free_assumptions_A57, float if_yes_enter_riskfree_rate_after_year10_A58, boolean override_long_term_growth_rate_assumptions_A60, float if_yes_enter_the_growth_rate_in_perpetuity_A61, boolean override_cash_is_a_neutral_asset_A63, float if_yes_enter_trapped_cash_or_entire_balance_A64, float average_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65) {
+        Override_stable_growth_A41 = override_stable_growth_A41;
+        If_yes_enter_the_cost_of_capital_after_year10_A42 = if_yes_enter_the_cost_of_capital_after_year10_A42;
+        Override_competitive_advantages_will_fade_A44 = override_competitive_advantages_will_fade_A44;
+        If_yes_enter_capital_you_expect_after_year10_A45 = if_yes_enter_capital_you_expect_after_year10_A45;
+        Override_firm_has_no_chance_of_failure_A47 = override_firm_has_no_chance_of_failure_A47;
+        If_yes_enter_the_probability_of_failure_A48 = if_yes_enter_the_probability_of_failure_A48;
+        What_do_you_want_to_tie_your_proceeds_in_failure_to_A49 = what_do_you_want_to_tie_your_proceeds_in_failure_to_A49;
+        Enter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50 = enter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50;
+        Override_long_term_tax_rate_assumption_A52 = override_long_term_tax_rate_assumption_A52;
+        Overrude_loss_tax_shield_assumption_A54 = overrude_loss_tax_shield_assumption_A54;
+        If_yes_enter_NOL_carrying_into_year1_A55 = if_yes_enter_NOL_carrying_into_year1_A55;
+        Override_long_term_risk_free_assumptions_A57 = override_long_term_risk_free_assumptions_A57;
+        If_yes_enter_riskfree_rate_after_year10_A58 = if_yes_enter_riskfree_rate_after_year10_A58;
+        Override_long_term_growth_rate_assumptions_A60 = override_long_term_growth_rate_assumptions_A60;
+        If_yes_enter_the_growth_rate_in_perpetuity_A61 = if_yes_enter_the_growth_rate_in_perpetuity_A61;
+        Override_cash_is_a_neutral_asset_A63 = override_cash_is_a_neutral_asset_A63;
+        If_yes_enter_trapped_cash_or_entire_balance_A64 = if_yes_enter_trapped_cash_or_entire_balance_A64;
+        Average_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65 = average_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65;
+    }
+
+    public boolean isOverride_stable_growth_A41() {
+        return Override_stable_growth_A41;
+    }
+
+    public void setOverride_stable_growth_A41(boolean override_stable_growth_A41) {
+        Override_stable_growth_A41 = override_stable_growth_A41;
+    }
+
+    public float getIf_yes_enter_the_cost_of_capital_after_year10_A42() {
+        return If_yes_enter_the_cost_of_capital_after_year10_A42;
+    }
+
+    public void setIf_yes_enter_the_cost_of_capital_after_year10_A42(float if_yes_enter_the_cost_of_capital_after_year10_A42) {
+        If_yes_enter_the_cost_of_capital_after_year10_A42 = if_yes_enter_the_cost_of_capital_after_year10_A42;
+    }
+
+    public boolean isOverride_competitive_advantages_will_fade_A44() {
+        return Override_competitive_advantages_will_fade_A44;
+    }
+
+    public void setOverride_competitive_advantages_will_fade_A44(boolean override_competitive_advantages_will_fade_A44) {
+        Override_competitive_advantages_will_fade_A44 = override_competitive_advantages_will_fade_A44;
+    }
+
+    public float getIf_yes_enter_capital_you_expect_after_year10_A45() {
+        return If_yes_enter_capital_you_expect_after_year10_A45;
+    }
+
+    public void setIf_yes_enter_capital_you_expect_after_year10_A45(float if_yes_enter_capital_you_expect_after_year10_A45) {
+        If_yes_enter_capital_you_expect_after_year10_A45 = if_yes_enter_capital_you_expect_after_year10_A45;
+    }
+
+    public boolean isOverride_firm_has_no_chance_of_failure_A47() {
+        return Override_firm_has_no_chance_of_failure_A47;
+    }
+
+    public void setOverride_firm_has_no_chance_of_failure_A47(boolean override_firm_has_no_chance_of_failure_A47) {
+        Override_firm_has_no_chance_of_failure_A47 = override_firm_has_no_chance_of_failure_A47;
+    }
+
+    public float getIf_yes_enter_the_probability_of_failure_A48() {
+        return If_yes_enter_the_probability_of_failure_A48;
+    }
+
+    public void setIf_yes_enter_the_probability_of_failure_A48(float if_yes_enter_the_probability_of_failure_A48) {
+        If_yes_enter_the_probability_of_failure_A48 = if_yes_enter_the_probability_of_failure_A48;
+    }
+
+    public String getWhat_do_you_want_to_tie_your_proceeds_in_failure_to_A49() {
+        return What_do_you_want_to_tie_your_proceeds_in_failure_to_A49;
+    }
+
+    public void setWhat_do_you_want_to_tie_your_proceeds_in_failure_to_A49(String what_do_you_want_to_tie_your_proceeds_in_failure_to_A49) {
+        What_do_you_want_to_tie_your_proceeds_in_failure_to_A49 = what_do_you_want_to_tie_your_proceeds_in_failure_to_A49;
+    }
+
+    public float getEnter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50() {
+        return Enter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50;
+    }
+
+    public void setEnter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50(float enter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50) {
+        Enter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50 = enter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50;
+    }
+
+    public boolean isOverride_long_term_tax_rate_assumption_A52() {
+        return Override_long_term_tax_rate_assumption_A52;
+    }
+
+    public void setOverride_long_term_tax_rate_assumption_A52(boolean override_long_term_tax_rate_assumption_A52) {
+        Override_long_term_tax_rate_assumption_A52 = override_long_term_tax_rate_assumption_A52;
+    }
+
+    public boolean isOverrude_loss_tax_shield_assumption_A54() {
+        return Overrude_loss_tax_shield_assumption_A54;
+    }
+
+    public void setOverrude_loss_tax_shield_assumption_A54(boolean overrude_loss_tax_shield_assumption_A54) {
+        Overrude_loss_tax_shield_assumption_A54 = overrude_loss_tax_shield_assumption_A54;
+    }
+
+    public float getIf_yes_enter_NOL_carrying_into_year1_A55() {
+        return If_yes_enter_NOL_carrying_into_year1_A55;
+    }
+
+    public void setIf_yes_enter_NOL_carrying_into_year1_A55(float if_yes_enter_NOL_carrying_into_year1_A55) {
+        If_yes_enter_NOL_carrying_into_year1_A55 = if_yes_enter_NOL_carrying_into_year1_A55;
+    }
+
+    public boolean isOverride_long_term_risk_free_assumptions_A57() {
+        return Override_long_term_risk_free_assumptions_A57;
+    }
+
+    public void setOverride_long_term_risk_free_assumptions_A57(boolean override_long_term_risk_free_assumptions_A57) {
+        Override_long_term_risk_free_assumptions_A57 = override_long_term_risk_free_assumptions_A57;
+    }
+
+    public float getIf_yes_enter_riskfree_rate_after_year10_A58() {
+        return If_yes_enter_riskfree_rate_after_year10_A58;
+    }
+
+    public void setIf_yes_enter_riskfree_rate_after_year10_A58(float if_yes_enter_riskfree_rate_after_year10_A58) {
+        If_yes_enter_riskfree_rate_after_year10_A58 = if_yes_enter_riskfree_rate_after_year10_A58;
+    }
+
+    public boolean isOverride_long_term_growth_rate_assumptions_A60() {
+        return Override_long_term_growth_rate_assumptions_A60;
+    }
+
+    public void setOverride_long_term_growth_rate_assumptions_A60(boolean override_long_term_growth_rate_assumptions_A60) {
+        Override_long_term_growth_rate_assumptions_A60 = override_long_term_growth_rate_assumptions_A60;
+    }
+
+    public float getIf_yes_enter_the_growth_rate_in_perpetuity_A61() {
+        return If_yes_enter_the_growth_rate_in_perpetuity_A61;
+    }
+
+    public void setIf_yes_enter_the_growth_rate_in_perpetuity_A61(float if_yes_enter_the_growth_rate_in_perpetuity_A61) {
+        If_yes_enter_the_growth_rate_in_perpetuity_A61 = if_yes_enter_the_growth_rate_in_perpetuity_A61;
+    }
+
+    public boolean isOverride_cash_is_a_neutral_asset_A63() {
+        return Override_cash_is_a_neutral_asset_A63;
+    }
+
+    public void setOverride_cash_is_a_neutral_asset_A63(boolean override_cash_is_a_neutral_asset_A63) {
+        Override_cash_is_a_neutral_asset_A63 = override_cash_is_a_neutral_asset_A63;
+    }
+
+    public float getIf_yes_enter_trapped_cash_or_entire_balance_A64() {
+        return If_yes_enter_trapped_cash_or_entire_balance_A64;
+    }
+
+    public void setIf_yes_enter_trapped_cash_or_entire_balance_A64(float if_yes_enter_trapped_cash_or_entire_balance_A64) {
+        If_yes_enter_trapped_cash_or_entire_balance_A64 = if_yes_enter_trapped_cash_or_entire_balance_A64;
+    }
+
+    public float getAverage_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65() {
+        return Average_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65;
+    }
+
+    public void setAverage_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65(float average_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65) {
+        Average_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65 = average_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65;
+    }
+
     private boolean Override_stable_growth_A41;
     private float If_yes_enter_the_cost_of_capital_after_year10_A42;
 
