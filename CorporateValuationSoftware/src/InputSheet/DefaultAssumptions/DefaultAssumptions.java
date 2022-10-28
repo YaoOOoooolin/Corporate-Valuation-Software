f42ab156fcedd8a39f1f7c6a7cf7ad1170bbef80
scykl1 <scykl1@nottingham.edu.cn>
2022-10-28 16:48:05 +0800

    Add the JAVA framework

---
+++ CorporateValuationSoftware/src/InputSheet/DefaultAssumptions/DefaultAssumptions.java
@@ -0,0 +1,47 @@
+package InputSheet.DefaultAssumptions;
+
+public class DefaultAssumptions {
+    private boolean Override_stable_growth_A41;
+    private float If_yes_enter_the_cost_of_capital_after_year10_A42;
+
+    private boolean Override_competitive_advantages_will_fade_A44;
+    private float If_yes_enter_capital_you_expect_after_year10_A45;
+
+    private boolean Override_firm_has_no_chance_of_failure_A47;
+    private float If_yes_enter_the_probability_of_failure_A48;
+
+    private String What_do_you_want_to_tie_your_proceeds_in_failure_to_A49;   //  (v or B)
+    private float Enter_the_distress_proceeds_as_percentage_of_book_or_fair_value_A50;
+
+    private boolean Override_long_term_tax_rate_assumption_A52;
+
+    private boolean Overrude_loss_tax_shield_assumption_A54;
+    private float If_yes_enter_NOL_carrying_into_year1_A55;
+
+    private boolean Override_long_term_risk_free_assumptions_A57;
+    private float If_yes_enter_riskfree_rate_after_year10_A58;
+
+    private boolean Override_long_term_growth_rate_assumptions_A60;
+    private float If_yes_enter_the_growth_rate_in_perpetuity_A61;
+
+    private boolean Override_cash_is_a_neutral_asset_A63;
+    private float If_yes_enter_trapped_cash_or_entire_balance_A64;
+    private float Average_tax_rate_of_the_foreign_markets_where_the_cash_is_trapped_A65;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ CorporateValuationSoftware/src/InputSheet/OtherInputs/OtherInputs.java
@@ -0,0 +1,13 @@
+package InputSheet.OtherInputs;
+
+public class OtherInputs {
+
+
+    private boolean Do_you_have_employee_options_outstanding_A33;
+    private float Number_of_options_outstanding_A34;
+    private float Average_strike_price_A35;
+    private float Average_maturity_A36;
+    private float Standard_deviation_on_stock_price_A37;
+
+
+}
