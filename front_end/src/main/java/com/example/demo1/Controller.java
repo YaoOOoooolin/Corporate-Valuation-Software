3053be1668701085ef685dcb131d3a75c560179d
scymw7 <scymw7@nottingham.edu.cn>
2023-02-22 11:34:58 +0800

    organize files and upadate front end

---
+++ front_end/src/main/java/com/example/demo1/Controller.java
@@ -0,0 +1,80 @@
+package com.example.demo1;
+
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.*;
+        import java.util.Locale;
+
+public class Controller {
+    @FXML
+    private Button TPtoInput1;
+
+    @FXML
+    private Button TPtoInput2;
+
+    @FXML
+    private Button TPtoInput3;
+
+    @FXML
+    private Button TPtoInput4;
+
+    @FXML
+    private Button TPtoInput5;
+    @FXML
+    private Tab tab1;
+    @FXML
+    private Tab tab2;
+    @FXML
+    private Tab tab5;
+
+    @FXML
+    private Tab tab3;
+    @FXML
+    private Tab tab4;
+
+    @FXML
+    private TabPane tabPane;
+
+
+    @FXML
+    private ComboBox<String> comboBox1;
+
+
+
+    @FXML
+    private void initialize() {
+        //ObservableList<String> observableList = FXCollections.observableArrayList("111", "222", "333");
+        //combo1.setItems(observableList);
+        comboBox1.getItems().addAll(
+                "United States",
+                "United Kingdom",
+                "China",
+                "Denmark",
+                "Japan");
+
+        Locale.setDefault(Locale.ENGLISH);
+    }
+
+    @FXML
+    void ToTab1(ActionEvent event) {
+        tabPane.getSelectionModel().select(tab1);
+    }
+
+    @FXML
+    void ToTab2(ActionEvent event) {
+        tabPane.getSelectionModel().select(tab2);
+    }
+
+    @FXML
+    void ToTab3(ActionEvent event) {
+        tabPane.getSelectionModel().select(tab3);
+    }
+
+    @FXML
+    void ToTab4(ActionEvent event) {
+        tabPane.getSelectionModel().select(tab4);
+    }
+    @FXML
+    void ToTab5(ActionEvent event) {tabPane.getSelectionModel().select(tab5);
+    }
+}
