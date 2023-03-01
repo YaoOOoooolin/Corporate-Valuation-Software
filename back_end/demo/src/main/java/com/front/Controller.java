d584d4e56971f7c23fb4a4d81799169958130ff9
scyzz9 <scyzz9@nottingham.edu.cn>
2023-03-01 11:44:21 +0800

    "Add new CorporateValuationFrontEnd.java"

--- back_end/demo/src/main/java/com/front/Controller.java
+++ back_end/demo/src/main/java/com/front/Controller.java
@@ -1,17 +1,71 @@
 package com.front;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-        import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Node;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.control.*;
-        import javafx.stage.Stage;
-        import java.io.IOException;
-        import java.util.Locale;
+import java.io.IOException;
+import java.util.Locale;
 
 public class Controller {
+    public DatePicker B1;
+    public TextField B2;
+    public TextField B8;
+    public TextField B9;
+    public TextField B10;
+    public TextField B11;
+    public TextField B12;
+    public TextField B15;
+    public TextField B16;
+    public TextField B17;
+    public TextField B18;
+    public TextField B19;
+    public TextField B20;
+    public CheckBox B13Yes;
+    public CheckBox B13No;
+    public TextField B21;
+    public TextField B23;
+    public TextField B24;
+    public TextField B25;
+    public TextField B26;
+    public TextField B27;
+    public TextField B28;
+    public TextField B30;
+    public TextField B31;
+    public TextField B34;
+    public TextField B35;
+    public TextField B36;
+    public TextField B37;
+    public CheckBox B33Yes;
+    public CheckBox B33No;
+    public TextField B42;
+    public TextField B45;
+    public CheckBox B41Yes;
+    public CheckBox B41No;
+    public CheckBox B44Yes;
+    public CheckBox B44No;
+    public CheckBox B47Yes;
+    public CheckBox B47No;
+    public TextField B49;
+    public TextField B48;
+    public TextField B50;
+    public TextField B55;
+    public TextField B59;
+    public CheckBox B54Yes;
+    public CheckBox B54No;
+    public CheckBox B58Yes;
+    public CheckBox B58No;
+    public CheckBox B60Yes;
+    public CheckBox B60No;
+    public CheckBox B63Yes;
+    public CheckBox B63No;
+    public TextField B61;
+    public TextField B64;
+    public TextField B65;
     @FXML
     private Button TPtoInput1;
 
@@ -50,8 +104,6 @@ public class Controller {
     @FXML
     private Button submit_id;
 
-
-
     @FXML
     private void initialize() {
         //ObservableList<String> observableList = FXCollections.observableArrayList("111", "222", "333");
@@ -88,6 +140,7 @@ public class Controller {
     @FXML
     void ToTab5(ActionEvent event) {tabPane.getSelectionModel().select(tab5);
     }
+
     @FXML
     void ToTab6(ActionEvent event){
         tabPane.getSelectionModel().select(tab6);
@@ -96,11 +149,10 @@ public class Controller {
     void result(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("result.fxml"));
         Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
-        Scene scene = new Scene(root, 1094, 800);
+        Scene scene = new Scene(root, 850, 800);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
     }
-
 }
