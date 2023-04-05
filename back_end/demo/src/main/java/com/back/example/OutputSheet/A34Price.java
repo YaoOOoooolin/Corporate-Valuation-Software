5190488b90d04c684f5c9d4723372a210503b9c4
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:09:23 +0800

    Update A34Price.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A34Price.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A34Price.java
@@ -1,3 +1,11 @@
+/**
+
+The A34Price class represents the price of a single share of a company.
+
+It accepts a single input parameter, which is the price of a single share, and sets it as the price for the company.
+
+The class also provides a getter method to retrieve the price of a single share.
+*/
 package com.back.example.OutputSheet;
 
 public class A34Price {
