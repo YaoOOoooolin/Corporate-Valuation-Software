e68149b5a8fa14f60d1e197e9dc501cfd7412ea7
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:07:52 +0800

    Update A32NumberOfShares.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A32NumberOfShares.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A32NumberOfShares.java
@@ -1,3 +1,11 @@
+/**
+
+The A32NumberOfShares class represents the number of shares of a company.
+
+It accepts a single input parameter, which is the number of shares, and sets it as the number of shares for the company.
+
+The class also provides a getter method to retrieve the number of shares.
+*/
 package com.back.example.OutputSheet;
 
 public class A32NumberOfShares {
