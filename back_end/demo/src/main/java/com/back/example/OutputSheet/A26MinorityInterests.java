ba6a193bf8f0047bf4a95181afd192da76e60a12
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:02:21 +0800

    Update A26MinorityInterests.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A26MinorityInterests.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A26MinorityInterests.java
@@ -1,3 +1,13 @@
+/**
+
+This class stores the amount of minority interests and provides methods to set and get the value of minority interests.
+This class has the following public methods:
+setMinorityInterests(double inputB17)
+This method takes in a double value which represents the book value of minority interests and sets it as
+the value of minority interests.
+getMinorityInterests()
+This method returns the value of minority interests stored in the minorityInterests variable.
+*/
 package com.back.example.OutputSheet;
 
 public class A26MinorityInterests {
