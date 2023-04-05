562925ac767e79e8c474b6740691de9b0fab6a2e
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:00:09 +0800

    Update A22ProbabilityOfFailure.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A22ProbabilityOfFailure.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A22ProbabilityOfFailure.java
@@ -1,3 +1,15 @@
+/**
+
+This class stores the probability of failure, which is used to calculate the adjusted present value (APV).
+This class has the following public methods:
+setProbability_failure(double inputB48, boolean inputB47)
+This method takes in a double value which represents the probability of failure,
+and a boolean value which represents if the probability of failure is overridden.
+If the probability of failure is overridden, the method sets the probability_failure to the inputB48 value.
+Otherwise, the probability_failure is set to 0.
+getProbability_failure()
+This method returns the probability of failure stored in the probability_failure variable.
+*/
 package com.back.example.OutputSheet;
 
 public class A22ProbabilityOfFailure {
