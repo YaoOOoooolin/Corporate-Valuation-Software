236691ca21ec7abdd52141489005e080f971cada
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:10:15 +0800

    Update A38SalesToCapitalRatio.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A38SalesToCapitalRatio.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A38SalesToCapitalRatio.java
@@ -1,3 +1,13 @@
+/**
+
+The A38SalesToCapitalRatio class represents the sales to capital ratio for a company over a period of time.
+
+It accepts a single input parameter, which is the sales to capital ratio for the first year, and creates an ArrayList
+
+to store the sales to capital ratio for each subsequent year. The class provides a setter method to set the sales to capital
+
+ratio for each year and a getter method to retrieve the ArrayList of sales to capital ratios.
+*/
 package com.back.example.OutputSheet;
 
 import java.util.ArrayList;
