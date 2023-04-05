a09dc3da4423c54d655f35d33e89bb0e3671fe18
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:03:27 +0800

    Update A28NonOperatingAssets.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A28NonOperatingAssets.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A28NonOperatingAssets.java
@@ -1,3 +1,14 @@
+
+/**
+
+This class stores the amount of non-operating assets and provides methods to set and get the value of non-operating assets.
+This class has the following public methods:
+setNonOperatingAssets(double inputB16)
+This method takes in a double value which represents the book value of non-operating assets and sets the inputB16
+as the value of non-operating assets.
+getNonOperatingAssets()
+This method returns the value of non-operating assets stored in the nonOperatingAssets variable.
+*/
 package com.back.example.OutputSheet;
 
 public class A28NonOperatingAssets {
