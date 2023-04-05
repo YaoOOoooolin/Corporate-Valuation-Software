055e79ae7469ab9d4f0ab038de71cf9c08564f80
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:01:21 +0800

    Update A24ValueOfOperatingAssets.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A24ValueOfOperatingAssets.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A24ValueOfOperatingAssets.java
@@ -1,3 +1,17 @@
+/**
+
+This class calculates the value of operating assets and stores it in a double variable.
+The value of operating assets is calculated based on the sum of present values of cash flow over 10 years,
+the present value of the terminal value, the proceeds if the firm fails, and the probability of failure.
+This class has the following public methods:
+setValueOfOperatingAssets(A21SumOfPV sumOfPV, A23ProceedsIfFirmFails proceedsIfFirmFails, A22ProbabilityOfFailure probailityOfFailure)
+This method takes in an object of A21SumOfPV class which contains the sum of present values of cash flow over 10 years
+and the present value of the terminal value, an object of A23ProceedsIfFirmFails class which contains the proceeds if the firm fails,
+and an object of A22ProbabilityOfFailure class which contains the probability of failure.
+The method then calculates the value of operating assets and stores it in a double variable.
+getValueOfOperatingAssets()
+This method returns the value of operating assets calculated by setValueOfOperatingAssets() method.
+*/
 package com.back.example.OutputSheet;
 
 import java.math.BigDecimal;
