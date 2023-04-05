78b1595c959d7966f5209340d6f6bdcff528bd1c
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 21:59:41 +0800

    Update A21SumOfPV.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A21SumOfPV.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A21SumOfPV.java
@@ -1,3 +1,17 @@
+/**
+
+This class calculates the sum of the present values of cash flow over 10 years and the present value of the terminal value
+and stores it in a double variable.
+This class has the following public methods:
+setSumOfPV(A20PVCFOver10Year pvcfOver10Year, A19PVTerminalValue pvTerminalValue)
+This method takes in an object of A20PVCFOver10Year class which contains the present value of cash flow over 10 years,
+and an object of A19PVTerminalValue class which contains the present value of the terminal value.
+The method then calculates the sum of the present values of cash flow over 10 years and the present value of the terminal value
+and stores it in a double variable.
+getSumOfPV()
+This method returns the sum of the present values of cash flow over 10 years and the present value of the terminal value
+calculated by setSumOfPV() method.
+*/
 package com.back.example.OutputSheet;
 
 import java.math.BigDecimal;
