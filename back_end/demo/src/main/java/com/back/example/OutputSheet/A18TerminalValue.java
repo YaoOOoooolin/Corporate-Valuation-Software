488a1b4f9f3977664a0ceef7228b7d8719fe22c7
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 21:57:43 +0800

    Update A18TerminalValue.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A18TerminalValue.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A18TerminalValue.java
@@ -1,3 +1,18 @@
+/**
+
+This class calculates the terminal value and stores it in a double variable.
+The terminal value is calculated as the terminal cash flow divided by the difference between
+the terminal cost of capital and the terminal revenue growth rate, both of which are obtained
+from A12CostOfCapital and A2RevenueGrowthRate classes respectively.
+This class has the following public methods:
+setTerminalvalue(A12CostOfCapital costOfCapital, A16TerminalCashFlow terminalCashFlow, A2RevenueGrowthRate revenueGrowthRate)
+This method takes in an object of A12CostOfCapital class which contains the terminal cost of capital value,
+an object of A16TerminalCashFlow class which contains the terminal cash flow value,
+and an object of A2RevenueGrowthRate class which contains the terminal revenue growth rate value.
+The method then calculates the terminal value and stores it in a double variable.
+getTerminalvalue()
+This method returns the terminal value calculated by setTerminalvalue() method.
+*/
 package com.back.example.OutputSheet;
 
 import java.math.BigDecimal;
