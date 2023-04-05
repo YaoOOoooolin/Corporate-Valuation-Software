d55bd375f58f248a03eeee590c6d91b7d33282a4
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:05:20 +0800

    Update A30ValueOfOptions.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A30ValueOfOptions.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A30ValueOfOptions.java
@@ -1,3 +1,16 @@
+/**
+
+A class to calculate the value of options in the output sheet of a financial model.
+This class provides methods to set and get the value of options, based on input parameters
+in the financial model's output sheet.
+The value of options is calculated based on whether there are options present, as indicated
+by the boolean inputB33, and the value of the options themselves, as indicated by the
+double OptionValueD27.
+Example usage:
+A30ValueOfOptions valueOfOptions = new A30ValueOfOptions();
+valueOfOptions.setValueOfOptions(true, 1000.0);
+double optionsValue = valueOfOptions.getValueOfOptions();
+*/
 package com.back.example.OutputSheet;
 
 public class A30ValueOfOptions {
