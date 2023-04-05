5269e831f8cc82f79651da2432519b02732a2aba
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 21:58:57 +0800

    Update A20PVCFOver10Year.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A20PVCFOver10Year.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A20PVCFOver10Year.java
@@ -1,3 +1,15 @@
+/**
+
+This class calculates the present value of cash flow over 10 years and stores it in a double variable.
+The present value is calculated as the sum of the present values of free cash flow to firm (FCFF)
+for the first 10 years, which is obtained from A14PVFCFF class.
+This class has the following public methods:
+setpVCFOver10Year(A14PVFCFF pv_FCFF)
+This method takes in an object of A14PVFCFF class which contains the present values of FCFF for all years.
+The method then calculates the present value of cash flow over 10 years and stores it in a double variable.
+getpVCFOver10Year()
+This method returns the present value of cash flow over 10 years calculated by setpVCFOver10Year() method.
+*/
 package com.back.example.OutputSheet;
 
 import java.math.BigDecimal;
