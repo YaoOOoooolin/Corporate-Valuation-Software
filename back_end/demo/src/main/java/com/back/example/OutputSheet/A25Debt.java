ef0c2a7261d55901746ef421d7f22f899ccc9782
YaoOOoooolin <108522692+YaoOOoooolin@users.noreply.github.com>
2023-04-05 22:01:52 +0800

    Update A25Debt.java

--- back_end/demo/src/main/java/com/back/example/OutputSheet/A25Debt.java
+++ back_end/demo/src/main/java/com/back/example/OutputSheet/A25Debt.java
@@ -1,3 +1,19 @@
+/**
+
+This class stores the amount of debt and provides methods to set and get the value of debt.
+This class has the following public methods:
+setDebt(boolean inputB14, double olcC28, double inputB12)
+This method takes in a boolean value which represents if the debt should include the book value of equity,
+a double value which represents the book value of debt, and a double value which represents the book value of equity.
+If the debt should include the book value of equity, the method adds the inputB12 and olcC28 values and
+sets the result as the value of debt. Otherwise, the method sets the inputB12 as the value of debt.
+getDebt()
+This method returns the value of debt stored in the debt variable.
+*/
+
+
+
+
 package com.back.example.OutputSheet;
 
 import java.math.BigDecimal;
